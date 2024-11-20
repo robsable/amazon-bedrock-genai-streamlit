cf51f19b04b78334e4d5c76b685c071707e491eb
Rob Sable <robsable@amazon.com>
2024-11-20 08:06:49 -0500

    fix case

--- app/Main_Menu.py
+++ app/Main_Menu.py
@@ -5,7 +5,7 @@ pg = st.navigation({"Main Menu" : [
     st.Page("demos/Product_Content_Generator.py", title="Product_Content_Generator", icon="✍️"),
     st.Page("demos/Product_Image_Reader.py", title="Product_Image_Reader", icon="📸"), 
     st.Page("demos/Product_Review_Summarizer.py", title="Product_Review_Summarizer", icon="📖"), 
-    st.Page("demos/Product_Assistant_Chat.py", title="Product_Assistant_Chat", icon="💬")
+    st.Page("demos/Product_Assistant_Chat.py", title="Product_Assistant_chat", icon="💬")
 ]})
 
 pg.run()
