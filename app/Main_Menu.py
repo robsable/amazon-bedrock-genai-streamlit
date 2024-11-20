912885a48b56de9e85f9f9787ee57bab4d72a72e
Rob Sable <robsable@amazon.com>
2024-11-20 13:46:05 -0500

    menu titles

--- app/Main_Menu.py
+++ app/Main_Menu.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 pg = st.navigation({"Main Menu" : [
     st.Page("Home.py", title="Home", icon="🏠"), 
-    st.Page("demos/Product_Content_Generator.py", title="Product_Content_Generator", icon="✍️"),
-    st.Page("demos/Product_Image_Reader.py", title="Product_Image_Reader", icon="📸"), 
-    st.Page("demos/Product_Review_Summarizer.py", title="Product_Review_Summarizer", icon="📖"), 
-    st.Page("demos/Product_Assistant_chat.py", title="Product_Assistant_Chat", icon="💬")
+    st.Page("demos/Product_Content_Generator.py", title="Product Content Generator", icon="✍️"),
+    st.Page("demos/Product_Image_Reader.py", title="Product Image Reader", icon="📸"), 
+    st.Page("demos/Product_Review_Summarizer.py", title="Product Review Summarizer", icon="📖"), 
+    st.Page("demos/Product_Assistant_chat.py", title="Product Assistant Chat", icon="💬")
 ]})
 
 pg.run()
